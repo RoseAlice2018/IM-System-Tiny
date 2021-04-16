0db0e7c7facfcd465a0b617da1269d68a412f599
RoseAlice2018 <1505243015@qq.com>
2021-04-16 09:36:56 +0800

    update 用户消息广播

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"sync"
+	"io"
 )
 
 type Server struct{
@@ -53,15 +54,31 @@ func (this *Server)Handler(conn net.Conn){
 	//当前连接业务
 	//fmt.Println("链接建立成功")
 
-	user := NewUser(conn)
+	user := NewUser(conn,this)
 
-	//用户上线，将用户添加到onlinemap中
-	this.mapLock.Lock()
-	this.OnlineMap[user.Name] = user
-	this.mapLock.Unlock()
+	user.Online()
 
-	//广播当前用户上线消息
-	this.BroadCast(user,"已上线")
+	//接受客户端发送的消息
+	go func(){
+		buf := make([]byte,4096)
+		for{
+			n,err := conn.Read(buf)
+			if n==0 {
+				user.Offline()
+				return
+			}
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			//提取用户的消息(去除'\n')
+			msg := string(buf[:n-1])
+
+			//用户针对msg进行消息处理
+			user.DoMessage(msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {
