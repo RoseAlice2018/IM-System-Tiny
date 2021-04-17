1ecf8bc77bc3f61f7ac48bc42b964a3cf6eb9cbc
RoseAlice2018 <1505243015@qq.com>
2021-04-17 22:07:18 +0800

    update v0.7 增加了超时强制踢出的功能

--- server.go
+++ server.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
-	"io"
+	"time"
 )
 
 type Server struct{
@@ -58,6 +59,8 @@ func (this *Server)Handler(conn net.Conn){
 
 	user.Online()
 
+	//监听用户是否活跃的channel
+	isLive := make(chan bool)
 	//接受客户端发送的消息
 	go func(){
 		buf := make([]byte,4096)
@@ -77,11 +80,35 @@ func (this *Server)Handler(conn net.Conn){
 
 			//用户针对msg进行消息处理
 			user.DoMessage(msg)
+
+			//来自用户的任意消息，代表当前用户是一个活跃的
+			isLive <- true
 		}
 	}()
 
 	//当前handler阻塞
-	select {
+	for{
+		select {
+			case <- isLive:
+				//当前用户是活跃的，应该重置定时器
+				//不做任何事情，为了激活select，更新下面的定时器
+				//
+				case <-time.After(time.Second * 10):
+					//已经超时
+					//将当前的User强制的关闭
+					user.SendMsg("你被踢了")
+
+					//销毁用的资源
+					close(user.C)
+
+					//关闭连接
+					conn.Close()
+
+					//退出当前Handler
+					return
+
+
+		}
 	}
 }
 
